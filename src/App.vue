2dac1874b3fd52566e73be476337bc1164931b1b
Isaias <jisolorioa@concredito.com.mx>
2017-11-12 18:38:59 -0700

    Cambios a amster

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-    <h1> Hello World! </h1>
+    <div class="container">
+        <app-header></app-header>
+        <div class="row">
+            <div class="col-xs-12">
+                <router-view> </router-view>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Header from './components/Header'
     export default {
-        
+        components: {
+            appHeader: Header
+        }
     }
 </script>
 
 <style>
-    
+    body {
+        padding: 30px;
+    }
 </style>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    NAME
+                    <small>(Price: PRICE)</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input type="number" class="form-control" placeholder="Quantity">
+                </div>
+                <div class="pull-right">
+                    <button class="btn btn-success">
+                            Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
